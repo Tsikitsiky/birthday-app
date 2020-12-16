9c45d8f9c0a9c8f967cd4a3933a5f10427fd221e
Sarah Razanatsoa <sarah.raz@onja.org>
2020-12-16 11:40:40 +0300

    change the api to fetch

--- libs.js
+++ libs.js
@@ -1,5 +1,5 @@
 // this function fetches all the people
-const endPoint = "https://gist.githubusercontent.com/Pinois/e1c72b75917985dc77f5c808e876b67f/raw/93debb7463fbaaec29622221b8f9e719bd5b119f/birthdayPeople.json";
+const endPoint = "https://gist.githubusercontent.com/Pinois/e1c72b75917985dc77f5c808e876b67f/raw/b17e08696906abeaac8bc260f57738eaa3f6abb1/birthdayPeople.json";
 export async function fetchData() {
     const response = await fetch(endPoint);
     const data = await response.json();
